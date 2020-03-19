eecf8d1c2fd675a3fc056d0da7443877e8df7b8d
Vahid Daneshmand <v.daneshmand@gmail.com>
2020-03-19 13:33:43 -0400

    Update flare-host.sh

--- flare-host.sh
+++ flare-host.sh
@@ -12,25 +12,29 @@ dpkg -s yq 2> /dev/null > /dev/null || (sudo apt-key adv --keyserver keyserver.u
 
 CONTAINER_NAME="flare-push-test"
 
-SHAREDDIRECTORY=$(yq r $CONFIGFILE shared-directory)
+CONFIG_FILE="flare-config.yml"
 
-APPDIRECTORY_DEFAULT="/root/app"
-APPDIRECTORY_GENERAL=$(yq r $CONFIGFILE app-directory)
+CONTAINER_SCRIPT="flare-container.sh"
+
+SHAREDDIRECTORY=$(yq r $CONFIG_FILE shared-directory)
+
+APPDIRECTORY_DEFAULT="/root/flare"
+APPDIRECTORY_GENERAL=$(yq r $CONFIG_FILE app-directory)
 APPDIRECTORY=$(set_value $APPDIRECTORY_GENERAL $APPDIRECTORY_DEFAULT)
 
 SSHKEY_PUBLIC_DEFAULT=$([[ $EUID -eq 0 ]] && echo "/root/.ssh/id_rsa.pub" || echo "/home/$USER/.ssh/id_rsa.pub")
-SSHKEY_PUBLIC_GENERAL=$(yq r $CONFIGFILE ssh-key.public)
-SSHKEY_PUBLIC_CONTAINER=$(yq r $CONFIGFILE $CONTAINER_NAME.git.ssh-key.public)
+SSHKEY_PUBLIC_GENERAL=$(yq r $CONFIG_FILE ssh-key.public)
+SSHKEY_PUBLIC_CONTAINER=$(yq r $CONFIG_FILE $CONTAINER_NAME.git.ssh-key.public)
 SSHKEY_PUBLIC=$(set_value $SSHKEY_PUBLIC_CONTAINER $SSHKEY_PUBLIC_GENERAL $SSHKEY_PUBLIC_DEFAULT)
 
 SSHKEY_PRIVATE_DEFAULT=$([[ $EUID -eq 0 ]] && echo "/root/.ssh/id_rsa" || echo "/home/$USER/.ssh/id_rsa")
-SSHKEY_PRIVATE_GENERAL=$(yq r $CONFIGFILE ssh-key.private)
-SSHKEY_PRIVATE_CONTAINER=$(yq r $CONFIGFILE $CONTAINER_NAME.git.ssh-key.private)
+SSHKEY_PRIVATE_GENERAL=$(yq r $CONFIG_FILE ssh-key.private)
+SSHKEY_PRIVATE_CONTAINER=$(yq r $CONFIG_FILE $CONTAINER_NAME.git.ssh-key.private)
 SSHKEY_PRIVATE=$(set_value $SSHKEY_PRIVATE_CONTAINER $SSHKEY_PRIVATE_GENERAL $SSHKEY_PRIVATE_DEFAULT)
 
 cp -u $SSHKEY_PRIVATE $SSHKEY_PUBLIC $SHAREDDIRECTORY
 
-DOCKER_RUN_COMMAND="docker run --rm -it -v $SHAREDDIRECTORY:$APPDIRECTORY $CONTAINER_NAME $APPDIRECTORY/script-container.sh"
+DOCKER_RUN_COMMAND="docker run --rm -it -v $SHAREDDIRECTORY:$APPDIRECTORY $CONTAINER_NAME $APPDIRECTORY/$CONTAINER_SCRIPT"
 
 # Run docker
 $DOCKER_RUN_COMMAND
