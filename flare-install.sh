6913c1a049267e3f2d1e189e10007987ff5159ff
Vahid Daneshmand <v.daneshmand@gmail.com>
2020-03-19 12:58:30 -0400

    Update flare-install.sh

--- flare-install.sh
+++ flare-install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Setup flare directory
 sudo mkdir -p /opt/flare
 sudo chown -R $USER:$USER /opt/flare
-wget -nc https://raw.githubusercontent.com/vahid-dan/flare-push-test/source/flare-config.yml -O /opt/flare/flare-config.yml
+
+# Download required scripts
+wget -c -O /opt/flare/ https://raw.githubusercontent.com/vahid-dan/flare-push-test/source/flare-config.yml
+wget -c -O /opt/flare/ https://raw.githubusercontent.com/vahid-dan/flare-push-test/source/flare-host.sh
+wget -c -O /opt/flare/ https://raw.githubusercontent.com/vahid-dan/flare-push-test/source/flare-container.sh
